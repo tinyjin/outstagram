0222ee6cae27722dab0b573e9594238a81c1539b
tinyjin <baram991103@gmail.com>
2019-07-16 20:28:29 +0900

    프로젝트 생성

---
+++ lib/main.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.black,
+          primaryIconTheme: IconThemeData(color: Colors.black),
+          primaryTextTheme: TextTheme(title: TextStyle(color: Colors.black)),
+          textTheme: TextTheme(title: TextStyle(color: Colors.black))),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final appBar = AppBar(
+    backgroundColor: Color(0xfff8faf8),
+    centerTitle: true,
+    elevation: 1.0,
+    leading: Icon(Icons.camera_alt),
+    title: SizedBox(
+        height: 35.0, child: Image.asset("assets/images/insta_logo.png")),
+    actions: <Widget>[
+      Padding(
+        padding: EdgeInsets.only(right: 12.0),
+        child: Icon(Icons.send),
+      )
+    ],
+  );
+
+  final navigationBar = Container(
+    color: Colors.white,
+    height: 50.0,
+    alignment: Alignment.center,
+    child: BottomAppBar(
+      child: Row(
+        // alignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.search,
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.add_box,
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.favorite,
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.account_box,
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+
+  final postList = Expanded(
+    child: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 16.0, top: 8.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          height: 40.0,
+                          width: 40.0,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: NetworkImage(
+                                    "https://cdn.pixabay.com/photo/2014/11/30/14/11/kitty-551554_1280.jpg")),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          "jinuiyou",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.more_vert),
+                      onPressed: null,
+                    )
+                  ],
+                ),
+              ),
+              Flexible(
+                fit: FlexFit.loose,
+                child: Image.network(
+                  "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Icon(
+                          Icons.favorite,
+                        ),
+                        SizedBox(
+                          width: 16.0,
+                        ),
+                        Icon(
+                          Icons.comment,
+                        ),
+                        SizedBox(
+                          width: 16.0,
+                        ),
+                        Icon(Icons.send),
+                      ],
+                    ),
+                    Icon(Icons.bookmark)
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  "Liked by wlsdml1103, tinyjin and 991,103 others",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      height: 25.0,
+                      width: 25.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: NetworkImage(
+                                "https://cdn.pixabay.com/photo/2014/11/30/14/11/kitty-551554_1280.jpg")),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        style: TextStyle(fontSize: 12.0),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Add a comment...",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  "1 Day Ago",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10.0,
+                  ),
+                ),
+              )
+            ],
+          );
+        }),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          postList,
+        ],
+      ), // This
+      bottomNavigationBar:
+          navigationBar, // trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
