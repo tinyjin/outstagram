ad300bb2f099983e6d906a905a3837dfe8a96eaa
tinyjin <baram991103@gmail.com>
2019-07-27 18:31:02 +0900

    좋아요 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -37,314 +37,194 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final appBar = AppBar(
-    backgroundColor: Color(0xfff8faf8),
-    centerTitle: true,
-    elevation: 1.0,
-    leading: Icon(Icons.camera_alt),
-    title: SizedBox(
-        height: 35.0, child: Image.asset("assets/images/insta_logo.png")),
-    actions: <Widget>[
-      Padding(
-        padding: EdgeInsets.only(right: 12.0),
-        child: Icon(Icons.send),
-      )
-    ],
-  );
+  var icon = Icons.favorite_border;
 
-  final navigationBar = Container(
-    color: Colors.white,
-    height: 50.0,
-    alignment: Alignment.center,
-    child: BottomAppBar(
+
+  @override
+  Widget build(BuildContext context) {
+    final appBar = AppBar(
+      backgroundColor: Color(0xfff8faf8),
+      centerTitle: true,
+      elevation: 1.0,
+      leading: Icon(Icons.camera_alt),
+      title: SizedBox(
+          height: 35.0, child: Image.asset("assets/images/insta_logo.png")),
+      actions: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(right: 12.0),
+          child: Icon(Icons.send),
+        )
+      ],
+    );
+
+    final navigationBar = Container(
+      color: Colors.white,
+      height: 50.0,
+      alignment: Alignment.center,
+      child: BottomAppBar(
+        child: Row(
+          // alignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.home,
+              ),
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.search,
+              ),
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.add_box,
+              ),
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.favorite,
+              ),
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.account_box,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    final profile = Padding(
+      padding: EdgeInsets.only(left: 16.0, top: 8.0, bottom: 8.0),
       child: Row(
-        // alignment: MainAxisAlignment.spaceAround,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.home,
-            ),
+          Row(
+            children: <Widget>[
+              Container(
+                height: 40.0,
+                width: 40.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(
+                          "https://cdn.pixabay.com/photo/2014/11/30/14/11/kitty-551554_1280.jpg")),
+                ),
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Text(
+                "jinuiyou",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              )
+            ],
           ),
           IconButton(
-            icon: Icon(
-              Icons.search,
+            icon: Icon(Icons.more_vert),
+            onPressed: null,
+          )
+        ],
+      ),
+    );
+
+    final image = Flexible(
+      child: Image.network(
+        "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg",
+      ),
+    );
+
+    final action = Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  print('hi');
+
+                  setState(() {
+                    icon = Icons.favorite;
+                  });
+                },
+                child: Icon(
+                  icon,
+                ),
+              ),
+              SizedBox(
+                width: 16.0,
+              ),
+              Icon(
+                Icons.chat_bubble_outline,
+              ),
+              SizedBox(
+                width: 16.0,
+              ),
+              Icon(Icons.send),
+            ],
+          ),
+          Icon(Icons.bookmark_border)
+        ],
+      ),
+    );
+
+    final likeText = Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Text(
+        "Liked by wlsdml1103, tinyjin and 991,103 others",
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+
+    final comment = Padding(
+      padding: EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 25.0,
+            width: 25.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                      "https://cdn.pixabay.com/photo/2014/11/30/14/11/kitty-551554_1280.jpg")),
             ),
           ),
-          IconButton(
-            icon: Icon(
-              Icons.add_box,
-            ),
+          SizedBox(
+            width: 10.0,
           ),
-          IconButton(
-            icon: Icon(
-              Icons.favorite,
-            ),
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.account_box,
+          Expanded(
+            child: TextField(
+              style: TextStyle(fontSize: 12.0),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Add a comment...",
+              ),
             ),
           ),
         ],
       ),
-    ),
-  );
+    );
 
-  final profile = Padding(
-    padding: EdgeInsets.only(left: 16.0, top: 8.0, bottom: 8.0),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Container(
-              height: 40.0,
-              width: 40.0,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: NetworkImage(
-                        "https://cdn.pixabay.com/photo/2014/11/30/14/11/kitty-551554_1280.jpg")),
-              ),
-            ),
-            SizedBox(
-              width: 10.0,
-            ),
-            Text(
-              "jinuiyou",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            )
-          ],
+    final date = Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Text(
+        "1 Day Ago",
+        style: TextStyle(
+          color: Colors.grey,
+          fontSize: 10.0,
         ),
-        IconButton(
-          icon: Icon(Icons.more_vert),
-          onPressed: null,
-        )
-      ],
-    ),
-  );
-
-  final image = Flexible(
-    child: Image.network(
-      "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg",
-    ),
-  );
-
-  final action = Padding(
-    padding: EdgeInsets.all(16.0),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Icon(
-              Icons.favorite_border,
-            ),
-            SizedBox(
-              width: 16.0,
-            ),
-            Icon(
-              Icons.chat_bubble_outline,
-            ),
-            SizedBox(
-              width: 16.0,
-            ),
-            Icon(Icons.send),
-          ],
-        ),
-        Icon(Icons.bookmark_border)
-      ],
-    ),
-  );
-
-  final likeText = Padding(
-    padding: EdgeInsets.symmetric(horizontal: 16.0),
-    child: Text(
-      "Liked by wlsdml1103, tinyjin and 991,103 others",
-      style: TextStyle(fontWeight: FontWeight.bold),
-    ),
-  );
-
-  final comment = Padding(
-    padding: EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        Container(
-          height: 25.0,
-          width: 25.0,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            image: DecorationImage(
-                fit: BoxFit.fill,
-                image: NetworkImage(
-                    "https://cdn.pixabay.com/photo/2014/11/30/14/11/kitty-551554_1280.jpg")),
-          ),
-        ),
-        SizedBox(
-          width: 10.0,
-        ),
-        Expanded(
-          child: TextField(
-            style: TextStyle(fontSize: 12.0),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: "Add a comment...",
-            ),
-          ),
-        ),
-      ],
-    ),
-  );
-
-  final date = Padding(
-    padding: EdgeInsets.symmetric(horizontal: 16.0),
-    child: Text(
-      "1 Day Ago",
-      style: TextStyle(
-        color: Colors.grey,
-        fontSize: 10.0,
       ),
-    ),
-  );
+    );
 
-
-
-  final postList1 = Expanded(
-    child: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(left: 16.0, top: 8.0, bottom: 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Container(
-                          height: 40.0,
-                          width: 40.0,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                                fit: BoxFit.fill,
-                                image: NetworkImage(
-                                    "https://cdn.pixabay.com/photo/2014/11/30/14/11/kitty-551554_1280.jpg")),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 10.0,
-                        ),
-                        Text(
-                          "jinuiyou",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        )
-                      ],
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.more_vert),
-                      onPressed: null,
-                    )
-                  ],
-                ),
-              ),
-              Flexible(
-                fit: FlexFit.loose,
-                child: Image.network(
-                  "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Icon(
-                          Icons.favorite_border,
-                        ),
-                        SizedBox(
-                          width: 16.0,
-                        ),
-                        Icon(
-                          Icons.chat_bubble_outline,
-                        ),
-                        SizedBox(
-                          width: 16.0,
-                        ),
-                        Icon(Icons.send),
-                      ],
-                    ),
-                    Icon(Icons.bookmark_border)
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  "Liked by wlsdml1103, tinyjin and 991,103 others",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      height: 25.0,
-                      width: 25.0,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            fit: BoxFit.fill,
-                            image: NetworkImage(
-                                "https://cdn.pixabay.com/photo/2014/11/30/14/11/kitty-551554_1280.jpg")),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Expanded(
-                      child: TextField(
-                        style: TextStyle(fontSize: 12.0),
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Add a comment...",
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  "1 Day Ago",
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 10.0,
-                  ),
-                ),
-              )
-            ],
-          );
-        }),
-  );
-
-  @override
-  Widget build(BuildContext context) {
     final post = Column(
       mainAxisAlignment: MainAxisAlignment.start,
       mainAxisSize: MainAxisSize.min,
